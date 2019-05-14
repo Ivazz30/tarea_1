9f602db922c8fd3a062cf4090284a65d6ade1487
Ivazz30 <ivannia.ramirez.maykall@gmail.com>
2019-05-13 20:27:24 -0600

    tarea_2_repository

---
+++ tarea_2.py
@@ -0,0 +1,11 @@
+#  Calcular el promedio de una lista de numeros
+
+values = [5, 6, 10, 13, 3, 4]
+sum_values = sum(values)
+len_values = len(values)
+prom = sum_values/len_values
+
+print "El promedio de todos los numeros de la lista es: %.2f" % prom
+
+# Calcular las alturas de diferentes gruoos de personas y determinar cual grupo posee la altura maxima.
+
