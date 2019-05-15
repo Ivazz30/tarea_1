9e03b6a389408aa9ef812a346cd86188db475197
Ivazz30 <ivannia.ramirez.maykall@gmail.com>
2019-05-14 18:19:49 -0600

    tarea_2_repository

--- tarea_3.py
+++ tarea_3.py
@@ -32,7 +32,8 @@ persona =[('Sofia',   'Alfaro',   32415116,   36, 51161161, 'consulta'),
           ('Carlos',  'Sanchez',  33411151,   15, 41655161, 'dolor'),
           ('Felipe',  'Perez',    12243151,   42, 65151111, 'documento'),
           ('Melissa', 'Alvarado', 734114144,  10, 87421312, 'dolor'),
-          ('Pedro',   'Castro',   4372124141, 2,  99313131, 'dolor'),]
+          ('Pedro',   'Castro',   4372124141, 02,  99313131, 'dolor')
+          ]
 
 agenda_hospital.extend(persona)
 for x in agenda_hospital:
@@ -71,3 +72,16 @@ for x in agenda_hospital:
     else:
         menor_edad += 1
 print "\n Hay %d pacientes mayores de edad y %d pacientes menores de edad" % (mayor_edad, menor_edad)
+
+# Pregunta 5. Ordenar primero por dolor y luego por edad.
+
+
+
+# Pregunta 6. Se mueren los que tienen dolor.
+
+# for x in agenda_hospital:
+  #  if x[5] != 'dolor':
+
+
+
+print agenda_hospital
